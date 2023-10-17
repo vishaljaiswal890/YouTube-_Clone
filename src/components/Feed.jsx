8d51860cc01d84ecbed2077d88c5adcac968a624
= <=>
2023-10-18 01:59:23 +0530

    Youtube_v_1

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -26,7 +26,7 @@ const Feed = () => {
           variant="body2"
           sx={{ mt: 1.5, color: "#fff" }}
         >
-          Copyright 2023 JSM Media
+          <b>Copyright 2023 Mediaplay</b>
         </Typography>
       </Box>
 
